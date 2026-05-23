f37089da628b4290ef7091ceb5c6709174f53f6e
nabendu82 <nabendu.biswas@gmail.com>
2026-05-23 13:32:52 +0530

    feat: implement Clerk authentication pages for sign-in and sign-up with styled background layout

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+    return (
+        <div className="min-h-screen bg-zinc-50 flex items-center justify-center p-4 relative overflow-hidden">
+            {/* Ambient background decoration */}
+            <div className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 rounded-full bg-purple-500/10 blur-[100px] pointer-events-none" />
+            <div className="absolute bottom-1/4 left-1/3 w-80 h-80 rounded-full bg-blue-500/5 blur-[100px] pointer-events-none" />
+            
+            <div className="relative z-10">
+                <SignUp />
+            </div>
+        </div>
+    );
+}
